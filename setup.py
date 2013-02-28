2f609b77a6c7917e73c9c227579e9a74aa086a37
Charles Law <charles.law@gmail.com>
2013-02-27 22:48:33 -0500

    Updated setup.py to work with PyPI; included additional required files in the MANIFEST

--- setup.py
+++ setup.py
@@ -2,20 +2,45 @@ from setuptools import setup, find_packages
 
 DESCRIPTION = 'Pythonic JavaScript syntax with support for advanced Python functionality'
 
-LONG_DESCRIPTION = None
-try:
-    LONG_DESCRIPTION = open('README.text').read()
-except:
-    pass
+LONG_DESCRIPTION = """
+Rapydscript
+===========
+
+RapydScript is a compiler that brings Pythonic syntax to JavaScript, 
+allowing you to write cleaner JavaScript and even reuse some Python
+code in your front-end.
+
+Installation
+------------
+To install Rapydscript simply use easy_install or pip:
+
+    pip install rapydscript
+
+or
+
+    easy_install rapydscript
+
+Rapydscript requires PyMeta, written by Waldemar Kornewald, which
+currently does not have a PyPI package.  It is included as part of the
+Rapydscript install.
+
+License
+-------
+The project is GPLv3, but the output is license free. 
+See http://www.gnu.org/licenses/gpl-faq.html#WhatCaseIsOutputGPL.
+
+"""
 
 setup(name='rapydscript',
-      packages=['rapyd'],
+      version='0.0.1',
+      packages=['rapyd', 'rapyd.pymeta'],
       package_data={'rapyd': ['*.pyj', '*.ometa']},
       author='Alexander Tsepkov',
-      url='http://www.pyjeon.com/projects/rapydscript',
+      url='http://rapydscript.pyjeon.com',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
+      license='GNU GPL3',
       install_requires=[],
       scripts=['rapydscript'],
       test_suite = "tests"
